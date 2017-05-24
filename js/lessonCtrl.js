0d012655cf67d64303a51f828bee728a773bcf86
Your Name <you@example.com>
2017-05-23 21:05:46 -0600

    Finished

--- js/lessonCtrl.js
+++ js/lessonCtrl.js
@@ -1,9 +1,7 @@
 angular.module("directivePractice").controller("lessonCtrl", function($scope,lessonService){
     $scope.lessons= ['Services', 'Routing', 'Directives', 'Review', 'Firebase', 'No server project', 'Node', 'Express', 'Mongo'];
     
- function announceDay(lesson, day){
-return alert(lesson + "is active on" + day + "." )
- $scope.announceDay = function (lesson, day){
+     $scope.announceDay = function (lesson, day){
         if(lesson && day){
         alert(lesson + ' is active on ' + day + '.')
         } else {
@@ -12,3 +10,5 @@ return alert(lesson + "is active on" + day + "." )
     }
 
 });
+
+
